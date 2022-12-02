c8e7077fb796ffeaeca418b85062b1fa898d6f32
Rashadul Haque <mohammed.haque@iths.se>
2022-12-02 17:53:36 +0100

    Updated

--- test/asyncExercise.test.ts
+++ test/asyncExercise.test.ts
@@ -6,22 +6,11 @@ import {
   getGroupsWithUsers,
 } from "../src/async";
 
-// Exercise 9 D1-2
-describe("Get users data", () => {
-  it("should return status == success", async () => {
-    const data = await fetchData(mockUsers);
-    expect(data.status).toBe("success");
-  });
-
-  it("should return data success", async () => {
-    const data = await fetchData(groups);
-    expect(data.status).toBe("success");
-  });
-});
 
 // Exercise 9, D1-2
-describe("Get users data", () => {
-  it("should return status == success", async () => {
+describe("Get users and groups data", () => {
+// Get data
+  it("should return users data as expected", async () => {
     const data = await fetchData(mockUsers);
     const expectedData = Object.values(data)[0];
     expect(expectedData).toEqual([
@@ -52,7 +41,7 @@ describe("Get users data", () => {
    ]);
   });
 
-  it("should return data success", async () => {
+  it("should return groups data as expected", async () => {
     const data = await fetchData(groups);
     const expectedData = Object.values(data)[0];
     expect(expectedData).toEqual([
@@ -70,11 +59,22 @@ describe("Get users data", () => {
       },
     ]);
   });
+
+// Get success status
+  it("should return user response status == success", async () => {
+    const data = await fetchData(mockUsers);
+    expect(data.status).toBe("success");
+  });
+
+  it("should return group response status == success", async () => {
+    const data = await fetchData(groups);
+    expect(data.status).toBe("success");
+  });
 });
 
 // Exercise 9, D3
 describe("Get group with users", () => {
-  it("should group with users:[name,...]", async () => {
+  it("should reformed group object with new key(users) as expected]", async () => {
     const data = await getGroupsWithUsers();
     expect(data).toEqual([
       { id: 1, groupName: "Hajarna", users: ["Erik", "Eva"] },
